f0668aa37a8aa9595f9523e125b751aa51c8887d
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-03-31 22:59:05 +0000

    Update page layout to include responsive padding and width

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CRTMonitor from "@/components/ui/crt-monitor";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex md:p-24">
+    <main className="min-h-screen flex xl:p-24 xl:w-5/6 w-full mx-auto">
       <div className="relative min-h-full min-w-full">
         <CRTMonitor>
           <div className="flex flex-col justify-center items-center h-full w-full">
