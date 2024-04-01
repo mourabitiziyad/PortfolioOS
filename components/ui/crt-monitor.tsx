0f53400b8157b6d358397a54be1dc0e5c7405e6a
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-01 01:11:33 +0000

    Update CRTMonitor component to use a blurred placeholder image

--- components/ui/crt-monitor.tsx
+++ components/ui/crt-monitor.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-function CRTMonitor({ children }: Readonly<{ children: React.ReactNode }>) {
+export function CRTMonitor({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<div className="bg-grey rounded-lg flex flex-wrap justify-center shadow-2xl border-2 h-full">
-			<Image placeholder='blur' src="/merzouga-full.jpg" alt="CRT Monitor" fill style={{objectFit: 'cover'}} className='-z-1' />
+			<Image placeholder='blur' blurDataURL='/merzouga-resized.jpg' src="/merzouga-full.jpg" alt="CRT Monitor" fill style={{ objectFit: 'cover' }} className='-z-1' />
 			<div className='bg-grey h-full w-full crt-glow scan-lines'></div>
 			<div className='absolute inset-0'>
 				<div className='absolute bottom-1 right-2 text-right font-light tracking-wide text-xs text-accent opacity-40 hover:opacity-100 transition duration-150 ease-in'>
@@ -19,8 +19,6 @@ function CRTMonitor({ children }: Readonly<{ children: React.ReactNode }>) {
 		</div>
 	);
 }
-
-export default CRTMonitor;
 
 
 // Morocco Photo by <a href="https://unsplash.com/@nelebki?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Niklas Schweinzer</a> on <a href="https://unsplash.com/photos/a-decorative-fountain-in-the-middle-of-a-courtyard-VqouWpsuziE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
