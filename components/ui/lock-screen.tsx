344f2bab8fa91b06e7620e03bb74e68e9a2c2c53
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-01 01:20:50 +0000

    Add WIP message

--- components/ui/lock-screen.tsx
+++ components/ui/lock-screen.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 export function LockScreen() {
 	return (
 		<div className="flex flex-col justify-center items-center h-full w-full">
+			<p className='rounded-full px-4 py-1 text-white text-base ring-1 ring-white mb-4 tracking-wide'>Currently Cooking</p>
 			<h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold text-white">PortfolioOS <span className="font-extralight">Merzouga</span></h1>
 			<p className="text-white"><button className="text-secondary">Click to continue {"->"}</button></p>
 		</div>
