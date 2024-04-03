93515254298565c51d515138dbcadf976b0dfede
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-03 21:56:02 +0000

    Add dynamic routing for different pages

---
+++ app/desktop/[folder]/page.tsx
@@ -0,0 +1,29 @@
+import { Window } from "@/components/window";
+
+function PageContent({ title }: Readonly<{ title: string }>) {
+  return (
+    <Window>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-gray-500">This is a simple example of a dynamic route.</p>
+      </div>
+    </Window>
+  );
+}
+
+export default function Page({ params }: Readonly<{ folder: string }>) {
+  switch (params.folder) {
+    case "projects":
+      return <PageContent title="Projects" />;
+    case "studies":
+      return <PageContent title="Studies" />;
+    case "about":
+      return <PageContent title="About" />;
+    default:
+      return <Window>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">File Not found {":("}</h1>
+      </div>
+    </Window>;
+  }
+}
