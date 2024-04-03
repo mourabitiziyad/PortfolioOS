7b66b39fcce995ed09e8cacf6015190844eb55ef
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-03 01:02:29 +0000

    Add motion animation to desktop view

--- components/ui/desktop-view.tsx
+++ components/ui/desktop-view.tsx
@@ -1,13 +1,53 @@
+"use client";
 import React from 'react';
 import { NavBar } from './navbar';
+import { Folder } from '../folder';
+import { motion } from 'framer-motion';
 
 export function DesktopView() {
+	const container = {
+		hidden: { opacity: 1, scale: 0 },
+		visible: {
+			opacity: 1,
+			scale: 1,
+			transition: {
+				delayChildren: 0.3,
+				staggerChildren: 0.2
+			}
+		}
+	}
+
+	const item = {
+		hidden: { x: -20, opacity: 0 },
+		visible: {
+			x: 0,
+			opacity: 1,
+			transition: {
+				duration: 0.2
+			}
+		}
+	}
 	return (
 		<div className='h-full w-full'>
 			<NavBar />
+			<div className='h-full xl:h-[97%] w-full flex justify-between flex-1'>
+				<motion.ul
+					className='h-full'
+					variants={container}
+					initial="hidden"
+					animate="visible"
+				>
+					{['Projects', 'Projects', 'Projects'].map((title, index) => (
+						<motion.li className='h-18' key={index} variants={item}>
+							<Folder title={title} />
+						</motion.li>
+					))}
+				</motion.ul>
+			</div>
 		</div>
 	);
 }
 
+// Folder design by https://dribbble.com/ashimadhikari, extracted multiple versions to the design to fit the project's needs
 
 // Morocco Photo by <a href="https://unsplash.com/@nelebki?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Niklas Schweinzer</a> on <a href="https://unsplash.com/photos/a-decorative-fountain-in-the-middle-of-a-courtyard-VqouWpsuziE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
