9df0c2695f9b4ccf064e44d80ec4c1569968a42b
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-03 00:58:42 +0000

    Remove unused code and simplify Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import { CRTMonitor } from "@/components/ui/crt-monitor";
-import { DesktopView } from "@/components/ui/desktop-view";
 import { LockScreen } from "@/components/ui/lock-screen";
+
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex xl:p-24 xl:w-5/6 w-full mx-auto">
-      <div className="relative min-h-full min-w-full">
-        <CRTMonitor>
-          <LockScreen />
-          {/* <DesktopView /> */}
-        </CRTMonitor>
-      </div>
-    </main>
+    <LockScreen />
   );
 }
