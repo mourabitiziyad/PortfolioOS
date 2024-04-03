edcd992192f258eff86209c3c5e45b06fcb82cf8
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-03 21:55:43 +0000

    Add navigation component

---
+++ navigation.tsx
@@ -0,0 +1,20 @@
+export const navigation = [
+	{
+		title: 'Ziyad',
+		id: 'ziyad',
+		path: '/about',
+		icon: 'user',
+	},
+	{
+		title: 'Studies',
+		id: 'studies',
+		path: '/studies',
+		icon: 'graduation-cap',
+	},
+	{
+		title: 'Projects',
+		id: 'projects',
+		path: '/projects',
+		icon: 'folder',
+	},
+]
