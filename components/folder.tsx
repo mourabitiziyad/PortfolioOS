04983c68648d9b3f1d57ae608e1a1799e9a8b91c
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-04 02:55:01 +0000

    Add FileIcon component to Folder component

--- components/folder.tsx
+++ components/folder.tsx
@@ -5,6 +5,7 @@ import { FolderOpenIcon } from './ui/folder-open-icon';
 import { FolderClosedIcon } from './ui/folder-closed-icon';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { FileIcon } from './ui/file-icon';
 
 export function Folder({ nav }: Readonly<{
   nav: {
@@ -24,11 +25,7 @@ export function Folder({ nav }: Readonly<{
     setIsOpen(path === `/desktop${nav.path}`)
   }, [path]);
 
-  const handleButtonClick = () => {
-    // setIsOpen(path === `/desktop${nav.path}`)
-  };
-
-  const variants = {
+  const folderVariants = {
     initial: {
       opacity: 0.1,
       scale: 0.5
@@ -56,38 +53,49 @@ export function Folder({ nav }: Readonly<{
       href={`/desktop${nav.path}`}
       className="flex flex-col items-center justify-center m-4 box-border h-20 w-16"
       tabIndex={0}
-      onClick={handleButtonClick}
     >
-      <LayoutGroup>
-        {isOpen ? (
-          <motion.div
-            className='will-change-transform will-change-opacity'
-            key="open"
-            style={{ willChange: 'auto' }}
-            variants={variants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-          >
-            {/* <Image className='h-50 w-50' loading='eager' placeholder='blur' blurDataURL='/folder-closed.svg' src={'/folder-open-paper.svg'} alt='Folder' width={50} height={50} /> */}
-            <FolderOpenIcon />
-          </motion.div>
+      {
+        nav.icon === 'folder' ? (
+          <LayoutGroup>
+            {isOpen ? (
+              <motion.div
+                className='will-change-transform will-change-opacity'
+                key="open"
+                style={{ willChange: 'auto' }}
+                variants={folderVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+              >
+                {/* <Image className='h-50 w-50' loading='eager' placeholder='blur' blurDataURL='/folder-closed.svg' src={'/folder-open-paper.svg'} alt='Folder' width={50} height={50} /> */}
+                <FolderOpenIcon />
+              </motion.div>
+            ) : (
+              <motion.div
+                className='will-change-transform will-change-opacity'
+                key="closed"
+                style={{ willChange: 'auto' }}
+                variants={folderVariants}
+                initial="initial"
+                animate="animate"
+                exit="exit"
+              >
+                {/* <Image className='h-50 w-50' loading="eager" src={'/folder-closed.svg'} alt='Folder' width={50} height={50} /> */}
+                <FolderClosedIcon />
+                {/* {path} /desktop{nav.path} */}
+              </motion.div>
+            )}
+          </LayoutGroup>
         ) : (
-          <motion.div
-            className='will-change-transform will-change-opacity'
-            key="closed"
+          <div
+            className='will-change-transform will-change-opacity p-2'
+            key="file"
             style={{ willChange: 'auto' }}
-            variants={variants}
-            initial="initial"
-            animate="animate"
-            exit="exit"
           >
-            {/* <Image className='h-50 w-50' loading="eager" src={'/folder-closed.svg'} alt='Folder' width={50} height={50} /> */}
-            <FolderClosedIcon />
-            {/* {path} /desktop{nav.path} */}
-          </motion.div>
-        )}
-      </LayoutGroup>
+            <FileIcon type={isOpen ? 'open' : 'closed'} />
+          </div>
+        )
+      }
       <p className='text-xs font-semiBold text-white'>{nav.title}</p>
     </Link>
   );
