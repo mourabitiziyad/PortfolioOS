bfa57ba51dbf3b26c044fd138ce8166eb96d8d6e
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-04 02:55:25 +0000

    Add file icon to MobileOnlyBar

--- components/ui/mobile-only-bar.tsx
+++ components/ui/mobile-only-bar.tsx
@@ -3,6 +3,7 @@ import { usePathname } from "next/navigation";
 import { FolderOpenIcon } from "./folder-open-icon";
 import Link from "next/link";
 import { navigation } from "@/navigation";
+import { FileIcon } from "./file-icon";
 
 export function MobileOnlyBar() {
   const path = usePathname();
@@ -14,7 +15,13 @@ export function MobileOnlyBar() {
       .filter((nav) => `/desktop${nav.path}` !== path)
       .map((nav) => (
         <Link href={`/desktop${nav.path}`} key={nav.id} className="gap-2 flex items-center bg-accent px-2 py-1 rounded-md hover:bg-slate-100">
-          <FolderOpenIcon height={20} width={20} />
+          {
+            nav.icon === "folder" ? (
+              <FolderOpenIcon height={20} width={20} />
+            ) : (
+              <FileIcon height={20} width={20} />
+            )
+          }
           <p className="text-black text-xs">{nav.title}</p>
         </Link>
       ))}
