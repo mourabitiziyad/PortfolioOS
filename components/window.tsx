3e016369ed4dd08cfc12cd849e3dd822a111e4c7
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-04 02:55:06 +0000

    Refactor window component structure

--- components/window.tsx
+++ components/window.tsx
@@ -6,15 +6,23 @@ export const Window = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <MotionDiv
-      className="absolute h-[90%] w-full self-center md:m-4 md:relative border-1 rounded-md bg-accent"
+      className="absolute h-[90%] w-full self-center md:m-4 md:relative md:h-full border-1 rounded-md bg-accent"
       initial={{ opacity: 0.5, y: -20 }}
       animate={{ opacity: 1, y: 0, scale: 1 }}
       exit={{ opacity: 0, y: 20 }}
       transition={{ duration: 0.5 }}
     >
-      <WindowBar />
-      <MobileOnlyBar />
-      {children}
+      <div className="h-full flex flex-col">
+        <div className="flex-none relative">
+          <WindowBar />
+        </div>
+        <div className="flex-none relative">
+          <MobileOnlyBar />
+        </div>
+        <div className="flex-1">
+          {children}
+        </div>
+      </div>
     </MotionDiv>
   );
 };
