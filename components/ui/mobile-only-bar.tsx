c8d4f64f7a0ba8fa8c36a4e6c59f2c2dda09fef6
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-04-04 18:52:51 +0000

    Add external icon to blog link in MobileOnlyBar component

--- components/ui/mobile-only-bar.tsx
+++ components/ui/mobile-only-bar.tsx
@@ -4,27 +4,32 @@ import { FolderOpenIcon } from "./folder-open-icon";
 import Link from "next/link";
 import { navigation } from "@/navigation";
 import { FileIcon } from "./file-icon";
+import Image from "next/image";
 
 export function MobileOnlyBar() {
   const path = usePathname();
   return (
     <div className="w-full px-4 bg-slate-300 py-2">
       <div className="flex gap-2 items-center">
-      <p className="text-black font-medium text-xs">Menus</p>
-      {navigation
-      .filter((nav) => `/desktop${nav.path}` !== path)
-      .map((nav) => (
-        <Link href={`/desktop${nav.path}`} key={nav.id} className="gap-2 flex items-center bg-accent px-2 py-1 rounded-md hover:bg-slate-100">
-          {
-            nav.icon === "folder" ? (
-              <FolderOpenIcon height={20} width={20} />
-            ) : (
-              <FileIcon height={20} width={20} />
+        <p className="text-black font-medium text-xs">Menus</p>
+        {navigation
+          .filter((nav) => `/desktop${nav.path}` !== path)
+          .map((nav) => (
+              <Link href={`/desktop${nav.path}`} key={nav.id} className="gap-2 flex items-center bg-accent px-2 py-1 rounded-md hover:bg-slate-100">
+                {
+                  nav.icon === "folder" ? (
+                    <FolderOpenIcon height={20} width={20} />
+                  ) : (
+                    <FileIcon height={20} width={20} />
+                  )
+                }
+                <div className="flex content-center">
+                  <p className="text-black text-xs mr-1">{nav.title}</p>
+                  {nav.path === 'blog' && <Image height={12} width={12} src="/external-icon.svg" alt="External" />}
+                </div>
+              </Link>
             )
-          }
-          <p className="text-black text-xs">{nav.title}</p>
-        </Link>
-      ))}
+          )}
       </div>
     </div>
   )
